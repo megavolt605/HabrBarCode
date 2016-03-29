02e92246f0a87068466c8581184f2c6efe9e2a88
Igor Smirnov <s@ronin.pro>
2016-03-29 12:09:48 +0300

    update to XCode 7.3, Swift 2.0

--- HabrBarCode/ViewController.swift
+++ HabrBarCode/ViewController.swift
@@ -24,27 +24,24 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     // это можно стелать и в другом месте, но я решил тут
     override func viewDidLoad() {
         super.viewDidLoad()
-        let path = NSBundle.mainBundle().pathForResource("chpok", ofType: "mp3")
-        if path == nil {
-            println("Файл со звуком не найден")
-            return
-        }
+        if let path = NSBundle.mainBundle().pathForResource("chpok", ofType: "mp3") {
+
+            if let url = NSURL(string: path) {
         
-        let url = NSURL(string: path!)
-        
-        var error: NSError?
-        
-        audioPlayer = AVAudioPlayer(contentsOfURL: url, error: &error)
-        if error != nil || audioPlayer == nil {
-            println("что-то со звуком не так")
-            if error != nil {
-                println(error!.description);
+                do {
+                    audioPlayer = try AVAudioPlayer(contentsOfURL: url)
+                } catch let error as NSError {
+                    print("что-то со звуком не так")
+                    print(error.description);
+                    audioPlayer = nil
+                    return
+                }
+                
+                audioPlayer?.prepareToPlay()
             }
-            return
+        } else {
+            print("Файл со звуком не найден")
         }
-        
-        audioPlayer!.prepareToPlay()
-        
     }
     
     func startRec() -> Bool {
@@ -52,61 +49,59 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
             return true
         }
         
-        var error: NSError?
         let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
-        let input = AVCaptureDeviceInput.deviceInputWithDevice(captureDevice, error: &error) as? AVCaptureDeviceInput
         
-        if input == nil {
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            // меняем текст кнопки
+            startStopButton.setTitle("Закончить", forState: .Normal)
+            
+            // настраиваем перехват и анализ видеопотока
+            
+            // перехват
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            let dispatchQueue = dispatch_queue_create("barCodeQueue", nil)
+            
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession!.addOutput(captureMetadataOutput)
+            
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue:dispatchQueue)
+            
+            // тут я перечислил все виды бар-куар-кодов, которые поддерживаются на текущий момент
+            captureMetadataOutput.metadataObjectTypes = [
+                AVMetadataObjectTypeQRCode,
+                AVMetadataObjectTypeUPCECode,
+                AVMetadataObjectTypeCode39Code,
+                AVMetadataObjectTypeCode39Mod43Code,
+                AVMetadataObjectTypeEAN13Code,
+                AVMetadataObjectTypeEAN8Code,
+                AVMetadataObjectTypeCode93Code,
+                AVMetadataObjectTypeCode128Code,
+                AVMetadataObjectTypePDF417Code,
+                AVMetadataObjectTypeAztecCode
+            ]
+            
+            // ну и добавляем стрим с камеры, чтобы пользователь видел что он снимает
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer!.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer!.frame = cameraView.layer.bounds
+            
+            cameraView.layer.addSublayer(videoPreviewLayer!)
+            
+            // show must go on!
+            captureSession!.startRunning()
+            
+            // ок, мы готовы
+            isActive = true
+            
+            return true
+        } catch let error as NSError {
             // по каким-то причинам мы не имеем доступ к камере, выходим
-            if error != nil {
-                println(error!.description)
-            }
+            print(error.description)
             return false
         }
         
-        // меняем текст кнопки
-        startStopButton.setTitle("Закончить", forState: .Normal)
-        
-        // настраиваем перехват и анализ видеопотока
-        
-        // перехват
-        captureSession = AVCaptureSession()
-        captureSession!.addInput(input)
-        let dispatchQueue = dispatch_queue_create("barCodeQueue", nil)
-        
-        let captureMetadataOutput = AVCaptureMetadataOutput()
-        captureSession!.addOutput(captureMetadataOutput)
-        
-        captureMetadataOutput.setMetadataObjectsDelegate(self, queue:dispatchQueue)
-        
-        // тут я перечислил все виды бар-куар-кодов, которые поддерживаются на текущий момент
-        captureMetadataOutput.metadataObjectTypes = [
-            AVMetadataObjectTypeQRCode,
-            AVMetadataObjectTypeUPCECode,
-            AVMetadataObjectTypeCode39Code,
-            AVMetadataObjectTypeCode39Mod43Code,
-            AVMetadataObjectTypeEAN13Code,
-            AVMetadataObjectTypeEAN8Code,
-            AVMetadataObjectTypeCode93Code,
-            AVMetadataObjectTypeCode128Code,
-            AVMetadataObjectTypePDF417Code,
-            AVMetadataObjectTypeAztecCode
-        ]
-        
-        // ну и добавляем стрим с камеры, чтобы пользователь видел что он снимает
-        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        videoPreviewLayer!.videoGravity = AVLayerVideoGravityResizeAspectFill
-        videoPreviewLayer!.frame = cameraView.layer.bounds
-        
-        cameraView.layer.addSublayer(videoPreviewLayer)
-        
-        // show must go on!
-        captureSession!.startRunning()
-        
-        // ок, мы готовы
-        isActive = true
-        
-        return true
     }
     
     func stopRec() -> Bool {
@@ -137,7 +132,7 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
     func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
         if metadataObjects != nil && metadataObjects.count > 0 {
-            let metadataObj = metadataObjects[0] as AVMetadataMachineReadableCodeObject
+            let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
             
             // меняем текст надписи в основном потоке
             dispatch_async(dispatch_get_main_queue()) {
